854c78b73c32c3400f2cb1c343b159cc16ea7b4c
FrontRunners <42584118+FrontRunners@users.noreply.github.com>
2018-08-22 22:54:47 +0530

    update 2-model

--- ContosoUniversity/Models/Course.cs
+++ ContosoUniversity/Models/Course.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-//comment
+//commented
 namespace ContosoUniversity.Models
 {
     public class Course
